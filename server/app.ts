47151911f7e4da4589c8a59a7c9692fb94d4bd79
zainulmustafa <zain2984@gmail.com>
2024-07-02 13:02:48 -0300

    posting the data on object and creating backup

--- server/app.ts
+++ server/app.ts
@@ -1,9 +1,21 @@
 import express from "express";
 import cors from "cors";
+import crypto from "crypto";
+import fs from "fs";
 
 const PORT = 8080;
 const app = express();
-const database = { data: "Hello World" };
+const database = { data: "Hello World", hash: "" };
+
+const generateHash = (data: string) => {
+  return crypto.createHash('sha256').update(data).digest('hex');
+};
+
+const backupData = () => {
+  fs.writeFileSync('backup.json', JSON.stringify(database));
+};
+
+database.hash = generateHash(database.data);
 
 app.use(cors());
 app.use(express.json());
@@ -16,6 +28,8 @@ app.get("/", (req, res) => {
 
 app.post("/", (req, res) => {
   database.data = req.body.data;
+  database.hash = generateHash(database.data);
+  backupData();
   res.sendStatus(200);
 });
 
