160a2923949741f623eb4ccbc2754aa101b3fe34
zainulmustafa <zain2984@gmail.com>
2024-07-02 13:16:59 -0300

    database shifted to json to test tampering

--- server/app.ts
+++ server/app.ts
@@ -4,48 +4,92 @@ import crypto from "crypto";
 import fs from "fs";
 
 const PORT = 8080;
+const DATABASE_FILE = 'database.json';
+const BACKUP_FILE = 'backup.json';
 const app = express();
-const database = { data: "Hello World", hash: "" };
-
-const generateHash = (data: string) => {
-  return crypto.createHash('sha256').update(data).digest('hex');
-};
-
-const backupData = () => {
-  fs.writeFileSync('backup.json', JSON.stringify(database));
-};
-
-const restoreData = () => {
-  const backup = JSON.parse(fs.readFileSync('backup.json', 'utf8'));
-  database.data = backup.data;
-  database.hash = backup.hash;
-};
-
-database.hash = generateHash(database.data);
 
 app.use(cors());
 app.use(express.json());
 
+interface Database {
+  data: string;
+  hash: string;
+}
+
+const readDatabase = (): Database => {
+  const data = fs.readFileSync(DATABASE_FILE, 'utf8');
+  return JSON.parse(data);
+};
+
+const writeDatabase = (database: Database): void => {
+  fs.writeFileSync(DATABASE_FILE, JSON.stringify(database, null, 2));
+};
+
+const readBackup = (): Database => {
+  const data = fs.readFileSync(BACKUP_FILE, 'utf8');
+  return JSON.parse(data);
+};
+
+const writeBackup = (database: Database): void => {
+  fs.writeFileSync(BACKUP_FILE, JSON.stringify(database, null, 2));
+};
+
+const generateHash = (data: string): string => {
+  return crypto.createHash('sha256').update(data).digest('hex');
+};
+
+const initializeDatabase = (): void => {
+  let database = readDatabase();
+  if (!database.hash) {
+    database.hash = generateHash(database.data);
+    writeDatabase(database);
+  }
+  
+  let backup = readBackup();
+  if (!backup.hash) {
+    backup.data = database.data;
+    backup.hash = database.hash;
+    writeBackup(backup);
+  }
+};
+
+initializeDatabase();
+
 // Routes
 
 app.get("/", (req, res) => {
+  const database = readDatabase();
   res.json(database);
 });
 
 app.post("/", (req, res) => {
+  const database = readDatabase();
   database.data = req.body.data;
   database.hash = generateHash(database.data);
-  backupData();
+  writeDatabase(database);
+  
+  const backup = readBackup();
+  backup.data = database.data;
+  backup.hash = database.hash;
+  writeBackup(backup);
+  
   res.sendStatus(200);
 });
 
 app.get("/verify", (req, res) => {
+  const database = readDatabase();
   const currentHash = generateHash(database.data);
   res.json({ valid: currentHash === database.hash });
 });
 
 app.post("/recover", (req, res) => {
-  restoreData();
+  const backup = readBackup();
+  const database = readDatabase();
+  
+  database.data = backup.data;
+  database.hash = backup.hash;
+  writeDatabase(database);
+  
   res.sendStatus(200);
 });
 
